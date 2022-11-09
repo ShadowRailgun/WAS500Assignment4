816295133b771da583cd4ec47d559dd31e88133b
Corinna Chang <corinnachang04@gmail.com>
2022-11-09 12:51:53 -0500

    adding post handlers and comments

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// Setting the port and required packages
 const port = 3000;
 const http = require("http");
 const httpStatusCodes = require("http-status-codes");
 const router = require("./router");
 const fs = require("fs");
 
+// Setting the different content types
 const plainTextContentType = {
   "Content-Type": "text/plain",
 };
@@ -17,6 +19,7 @@ const jpgContentType = {
   "Content-Type":"text/jpg",
 }
 
+// Read file for data or display error if issue with file
 const customReadFile = (file, res) => {
   fs.readFile(`./${file}`, (errors, data) => {
     if (errors) {
@@ -26,15 +29,17 @@ const customReadFile = (file, res) => {
   });
 };
 
+// Setting what the root request should look for
 router.get("/", (req, res) => {
   res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
   customReadFile("views/index.html", res);
 });
+
+// The different pages of the website
 router.get("/index.html", (req, res) => {
   res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
   customReadFile("views/index.html", res);
 });
-
 router.get("/books.html", (req, res) => {
   res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
   customReadFile("views/books.html", res);
@@ -51,16 +56,18 @@ router.get("/book3.html", (req, res) => {
   res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
   customReadFile("views/book3.html", res);
 });
-
 router.get("/honesty.html", (req, res) => {
   res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
   customReadFile("views/honesty.html", res);
 });
 
+// The style sheet for the website
 router.get("/public/css/styles.css", (req, res) => {
   res.writeHead(httpStatusCodes.StatusCodes.OK, cssContentType);
   customReadFile("public/css/styles.css", res);
 });
+
+// The images for the website
 router.get("/images/oathbreaker.jpg", (req, res) => {
   res.writeHead(httpStatusCodes.StatusCodes.OK, jpgContentType);
   customReadFile("public/images/oathbreaker.jpg", res);
@@ -74,9 +81,58 @@ router.get("/images/wool.jpg", (req, res) => {
   customReadFile("public/images/wool.jpg", res);
 });
 
+// The POST version
 router.post("/", (req, res) => {
-  res.writeHead(httpStatusCodes.StatusCodes.OK, plainTextContentType);
-  res.end("POSTED");
+  res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
+  customReadFile("views/index.html", res);
 });
+// The different pages of the website
+router.post("/index.html", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
+  customReadFile("views/index.html", res);
+});
+router.post("/books.html", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
+  customReadFile("views/books.html", res);
+});
+router.post("/book1.html", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
+  customReadFile("views/book1.html", res);
+});
+router.post("/book2.html", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
+  customReadFile("views/book2.html", res);
+});
+router.post("/book3.html", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
+  customReadFile("views/book3.html", res);
+});
+router.post("/honesty.html", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, htmlContentType);
+  customReadFile("views/honesty.html", res);
+});
+
+// The style sheet for the website
+router.post("/public/css/styles.css", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, cssContentType);
+  customReadFile("public/css/styles.css", res);
+});
+
+// The images for the website
+router.post("/images/oathbreaker.jpg", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, jpgContentType);
+  customReadFile("public/images/oathbreaker.jpg", res);
+});
+router.post("/images/ruby.jpg", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, jpgContentType);
+  customReadFile("public/images/ruby.jpg", res);
+});
+router.post("/images/wool.jpg", (req, res) => {
+  res.writeHead(httpStatusCodes.StatusCodes.OK, jpgContentType);
+  customReadFile("public/images/wool.jpg", res);
+});
+
+// Create the server listener
 http.createServer(router.handle).listen(3000);
+// Print to console that the server has started listening
 console.log(`The server is listening on port number: ${port}`);
